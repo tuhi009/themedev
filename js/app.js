09a19e9c0b51c110bb0e993be087692b082e19e3
tuhi009 <tuhi009@live.com>
2019-09-10 17:07:41 +0600

    merged from ashik. start of final updates

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(function () {
     $('.testimonial_section').owlCarousel({
         loop: true,
         margin: 10,
-        autoplay:true,
+        autoplay: true,
         nav: true,
         responsive: {
             0: {
@@ -35,7 +35,7 @@ $(function () {
             }
         },
         navText: ["<i class='text-danger fa fa-chevron-left'></i>", "<i class='text-danger fa fa-chevron-right'></i>"]
-        
+
     })
 
 
@@ -70,25 +70,84 @@ $(function () {
 
 $('.testimonial_slide').owlCarousel({
     loop: true,
-            margin: 10,
-             autoplay:true,
-             autoplayTimeout:1000,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: true
-                },
-                600: {
-                    items: 2,
-                    nav: false
-                },
-                1000: {
-                    items: 3,
-                    nav: true,
-                    loop: true
-                }
-            },
-            navText: ["<i class='text-danger fa fa-chevron-left'></i>", "<i class='text-danger fa fa-chevron-right'></i>"]
+    margin: 10,
+    autoplay: false,
+    autoplayTimeout: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: true
+        }
+    },
+    navText: ["<i class='text-danger fa fa-chevron-left'></i>", "<i class='text-danger fa fa-chevron-right'></i>"]
+
+});
+
+
+//<!-- text changer js -->
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 40,
+        backSpeed: 10,
+        startDelay: 1000,
+        loop: true,
+        loopCount: Infinity,
+        onComplete: function (self) {
+            prettyLog('onComplete ' + self);
+        },
+        preStringTyped: function (pos, self) {
+            prettyLog('preStringTyped ' + pos + ' ' + self);
+        },
+        onStringTyped: function (pos, self) {
+            prettyLog('onStringTyped ' + pos + ' ' + self);
+        },
+        onLastStringBackspaced: function (self) {
+            prettyLog('onLastStringBackspaced ' + self);
+        },
+        onTypingPaused: function (pos, self) {
+            prettyLog('onTypingPaused ' + pos + ' ' + self);
+        },
+        onTypingResumed: function (pos, self) {
+            prettyLog('onTypingResumed ' + pos + ' ' + self);
+        },
+        onReset: function (self) {
+            prettyLog('onReset ' + self);
+        },
+        onStop: function (pos, self) {
+            prettyLog('onStop ' + pos + ' ' + self);
+        },
+        onStart: function (pos, self) {
+            prettyLog('onStart ' + pos + ' ' + self);
+        },
+        onDestroy: function (self) {
+            prettyLog('onDestroy ' + self);
+        }
+    });
     
-        });
+});
+
+function prettyLog(str) {
+    console.log('%c ' + str, 'color: green; font-weight: bold;');
+}
+
+function toggleLoop(typed) {
+    if (typed.loop) {
+        typed.loop = false;
+    } else {
+        typed.loop = true;
+    }
+}
+
+
+//slider
