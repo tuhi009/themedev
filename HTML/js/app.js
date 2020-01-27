16c8a4b11ac6c0bd30764518f7b86ade2ce0281b
tuhi009 <tuhi009@live.com>
2020-01-27 18:03:58 +0600

    updated to the latest version restructured the folder for documentation

--- HTML/js/app.js
+++ HTML/js/app.js
@@ -94,13 +94,14 @@ $('.testimonial_slide').owlCarousel({
 });
 
 
-//<!-- text changer js -->
+//<!-- text changer js typed.js-->
 document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
-        typeSpeed: 40,
+        typeSpeed: 50,
         backSpeed: 10,
-        startDelay: 1000,
+        startDelay:500,
+        endDelay:1000,
         loop: true,
         loopCount: Infinity,
         onComplete: function (self) {
